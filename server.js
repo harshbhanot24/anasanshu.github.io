d3b6c8c951046b31939914fb2a25900f2d7858ab
anasanshu68 <anasanshu68@gmail.com>
2019-02-10 04:58:45 +0530

    Prepare for heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname+'/dist/portfolio'));
+
+app.all('*', (req,res) => {
+    res.status(200).sendfile(__dirname+'/dist/portfolio/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
